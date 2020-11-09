5cc28af0b36c4a47e9aaf5b14530b1f9aeb0e892
Chris Scott <cam022007@coderacademy.edu.au>
2020-11-08 20:57:01 -0800

    added some basic crud functionality to profiles controller as well as set up a link to a basic profile creation page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,22 @@
 class ProfilesController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :set_profile, only: [:show, :edit, :update]
+
   def show
   end 
 
   def new
+    @profile = Profile.new
   end 
 
   def create
+    @profile = current_user.profile.new(profile_params)
+    @profile.user = current_user
+    if @profile.save
+      redirect_to @profile
+    else 
+      render :new
+    end 
   end 
 
   def edit
@@ -18,9 +28,13 @@ class ProfilesController < ApplicationController
   private 
 
   def set_profile
+    id = params[:id]
+    @profile = Profile.find(id)
   end 
 
-  def profile_params
+  def listing_params
+    params.require(:profile).permit(:name, :description, :place_of_business, :about_me, :skills)
   end 
 
 end
+
--- app/models/tradesman.rb
+++ app/models/tradesman.rb
@@ -1,9 +1,5 @@
 class Tradesman < ApplicationRecord
   belongs_to :user
   has_one_attached :certification
-
-  def tradesman?
-    self.tradesman.nil?
-  end 
 end
 
