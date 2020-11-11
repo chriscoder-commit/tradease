e17e768a8b3b1cf46d7ed62223b435cc78d50062
Chris Scott <cam022007@coderacademy.edu.au>
2020-11-10 19:18:28 -0800

    comments now functioning in basic form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,18 +7,20 @@ class CommentsController < ApplicationController
 
   def create
     @listing = Listing.find(params[:listing_id])
-    @comment = @listing.comments.new(comment_params)
+    @comment = @listing.comments.create(comment_params)
+    @comment.user = current_user
+    p @comment.errors.full_messages
     if @comment.save
-      redirect_to listings_path
+      redirect_to @listing
     else
-      render :new 
+      redirect_to listings_path
     end 
   end 
 
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to listings_path 
+    redirect_to listing_comments_path
   end 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_053536) do
+ActiveRecord::Schema.define(version: 2020_11_11_030044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,9 @@ ActiveRecord::Schema.define(version: 2020_11_10_053536) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "listing_id", null: false
+    t.bigint "user_id", null: false
     t.index ["listing_id"], name: "index_comments_on_listing_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "listings", force: :cascade do |t|
@@ -96,6 +98,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_053536) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "listings"
+  add_foreign_key "comments", "users"
   add_foreign_key "listings", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "tradesmen", "users"
