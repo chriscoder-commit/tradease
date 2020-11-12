16f0e13dfa9cf724b46b30921c56b5f9315b9243
Chris Scott <cam022007@coderacademy.edu.au>
2020-11-11 22:00:09 -0800

    edited mailer

--- app/mailers/listing_mailer.rb
+++ app/mailers/listing_mailer.rb
@@ -2,6 +2,6 @@ class ListingMailer < ApplicationMailer
   
   def send_listing_new_mail(user)
     @user = user
-    mail( :to => @user.email, subject: => 'Thanks for expressing interest in this listing')
+    mail( :to => @user.email, :subject => 'Thanks for expressing interest in this listing')
   end
 end
