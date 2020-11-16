ed119ca488543b63c058c41a99e5485184e850f9
Chris Scott <cam022007@coderacademy.edu.au>
2020-11-15 23:33:57 -0800

    added enum finally

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,13 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_many :reviews
 
+  enum skills: {
+    "Carpentry": 0,
+    "Welding": 1,
+    "Landscaping": 2,
+    "Plumbing": 3,
+    "Electrician": 4,
+  }
+
   validates :name, presence: true
 end
