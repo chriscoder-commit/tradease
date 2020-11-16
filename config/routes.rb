a698b45b96dc0dab43b4706fe52248603ce25a95
Chris Scott <cam022007@coderacademy.edu.au>
2020-11-15 21:47:13 -0800

    added a landing page and some basic functionality so it can log people in and sign them up, and some logic so the nav doesn't appear

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "listings#index"
+  root to: "welcome#landing"
 
   resources :listings do
     resources :comments, only: [:new, :create, :destroy]
@@ -11,12 +11,6 @@ Rails.application.routes.draw do
       end
       resources :reviews, only: [:new, :create, :destroy]
     end
-  
-
-  
-
-
-  
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
